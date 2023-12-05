051776dc509dc0fadef014008b81b2908b514f73
Pratham1962 <148983275+Pratham1962@users.noreply.github.com>
2023-12-05 18:43:39 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 06, 2023',
+  birthdate: 'Dec 06, 2022',
   name: 'Delisha'
 };
 
